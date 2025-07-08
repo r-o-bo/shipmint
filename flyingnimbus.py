ffdcae78001770750ad37f7a1befb2f13b4816c6
r-o-bo <ther0b0tombstone@gmail.com>
2025-07-08 20:14:57 +0530

    experiments

--- flyingnimbus.py
+++ flyingnimbus.py
@@ -1,3 +1,18 @@
 import psycopg2 as pg2
+import Flask
+import os
 
+print("hello world")
+
+# connection with postgres
 conn = pg2.connect(database='dvdrental', user='postgres',password='satu')
+
+
+cur = conn.cursor()
+cur.execute("SELECT * FROM payment")
+rows = cur.fetchmany(10)
+for item in rows:
+    print(item)
+
+cur.close()
+conn.close()
