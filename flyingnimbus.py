700715137ecca8a6db377e813982da3d964297a8
r-o-bo <ther0b0tombstone@gmail.com>
2025-07-09 23:54:46 +0530

    added schema kinda

--- flyingnimbus.py
+++ flyingnimbus.py
@@ -1,5 +1,4 @@
 import psycopg2 as pg2
-import Flask
 import os
 
 print("hello world")
@@ -11,6 +10,10 @@ conn = pg2.connect(database='dvdrental', user='postgres',password='satu')
 cur = conn.cursor()
 cur.execute("SELECT * FROM payment")
 rows = cur.fetchmany(10)
+
+# schema
+columns = [desc[0] for desc in cur.description]
+print(columns)
 for item in rows:
     print(item)
 
