d774d540212ada809bd3ac0cdba8e3c4074e75d5
r-o-bo <ther0b0tombstone@gmail.com>
2025-07-22 01:13:57 +0530

    rebuilt the script fix security issues

--- flyingnimbus.py
+++ flyingnimbus.py
@@ -6,18 +6,19 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # connection with postgres
-conn = pg2.connect(
-    host=os.getenv('DB_HOST'),
-    database=os.getenv('DB_NAME'),
-    user=os.getenv('DB_USER'),
-    password=os.getenv('DB_PASSWORD')
-)
+def get_connection():
+    conn = pg2.connect(
+        host=os.getenv('DB_HOST'),
+        database=os.getenv('DB_NAME'),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD')
+    )
+    return conn, conn.cursor()
 
-cur = conn.cursor()
 table_list = ["actor", "film", "film_actor", "category", "film_category", "store", "inventory", "rental", "payment", "staff",
               "customer", "address", "city", "country"]
 
-def your_query(query: str, filename: str = None) -> str:
+def your_query(query: str, cur, conn, filename: str = None) -> tuple:
     your_query.calls += 1
     print(f"\n Query result {your_query.calls}:\n")
 
@@ -49,64 +50,78 @@ def your_query(query: str, filename: str = None) -> str:
 
 your_query.calls = 0
 
-# Top 5 customers by Total Payment
-your_query("""SELECT cust.customer_id, cust.first_name, cust.last_name, SUM(pay.amount) AS total_paid
-              FROM customer cust
-              JOIN payment pay ON cust.customer_id = pay.customer_id
-              GROUP BY cust.customer_id, cust.first_name, cust.last_name
-              ORDER BY total_paid DESC
-              LIMIT 5""", "top5.csv")
+if __name__ == "__main__":
 
-# Customers who have rented films in the 'Action' category
-your_query("""SELECT DISTINCT c.customer_id, c.first_name, c.last_name
-              FROM customer c
-              JOIN rental r ON c.customer_id = r.customer_id
-              JOIN inventory i ON r.inventory_id = i.inventory_id
-              JOIN film_category fc ON i.film_id = fc.film_id
-              JOIN category cat ON fc.category_id = cat.category_id
-              WHERE cat.name = 'Action'
-              LIMIT 5        
-""","action.csv")
+     # tuple unpacks the shit
+    conn, cur = get_connection()
 
-# Customers who never made a payment
-your_query("""SELECT c.customer_id, c.first_name, c.last_name
-FROM customer c
-LEFT JOIN payment p ON c.customer_id = p.customer_id
-WHERE p.payment_id IS NULL
-LIMIT 5
-""","nopayment.csv")
+    # Top 5 customers by Total Payment
+    your_query("""SELECT cust.customer_id, cust.first_name, cust.last_name, SUM(pay.amount) AS total_paid
+                FROM customer cust
+                JOIN payment pay ON cust.customer_id = pay.customer_id
+                GROUP BY cust.customer_id, cust.first_name, cust.last_name
+                ORDER BY total_paid DESC
+                LIMIT 5""", cur, conn, "top5.csv")
 
-# Movies that were never rented
-your_query("""SELECT f.film_id, f.title
-FROM film f
-LEFT JOIN inventory i ON f.film_id = i.film_id
-LEFT JOIN rental r ON i.inventory_id = r.inventory_id
-WHERE r.rental_id IS NULL
-LIMIT 5
-""","norent.csv")
+    # Customers who have rented films in the 'Action' category
+    your_query("""SELECT DISTINCT c.customer_id, c.first_name, c.last_name
+                FROM customer c
+                JOIN rental r ON c.customer_id = r.customer_id
+                JOIN inventory i ON r.inventory_id = i.inventory_id
+                JOIN film_category fc ON i.film_id = fc.film_id
+                JOIN category cat ON fc.category_id = cat.category_id
+                WHERE cat.name = 'Action'
+                LIMIT 5        
+    """, cur, conn,"action.csv")
 
-# Label customers based on how much money they have spent
-your_query("""SELECT 
-    c.customer_id,
-    c.first_name,
-    c.last_name,
-    COALESCE(SUM(p.amount), 0) AS total_paid,
-    
-    CASE 
-        WHEN COALESCE(SUM(p.amount), 0) >= 100 THEN 'High $$$'
-        WHEN COALESCE(SUM(p.amount), 0) BETWEEN 50 AND 99.99 THEN 'Medium $$'
-        WHEN COALESCE(SUM(p.amount), 0) BETWEEN 1 AND 49.99 THEN 'Low $'
-        ELSE 'No Payment'
-    END AS spending_tier
+    # Customers who never made a payment
+    your_query("""SELECT c.customer_id, c.first_name, c.last_name
+    FROM customer c
+    LEFT JOIN payment p ON c.customer_id = p.customer_id
+    WHERE p.payment_id IS NULL
+    LIMIT 5
+    """, cur, conn,"nopayment.csv")
 
-FROM customer c
-LEFT JOIN payment p ON c.customer_id = p.customer_id
-GROUP BY c.customer_id, c.first_name, c.last_name
-ORDER BY total_paid DESC
-LIMIT 5
-""","spending_tiers.csv")
+    # Movies that were never rented
+    your_query("""SELECT f.film_id, f.title
+    FROM film f
+    LEFT JOIN inventory i ON f.film_id = i.film_id
+    LEFT JOIN rental r ON i.inventory_id = r.inventory_id
+    WHERE r.rental_id IS NULL
+    LIMIT 5
+    """, cur, conn,"norent.csv")
 
-cur.close()
-conn.close()
+    # Label customers based on how much money they have spent
+    your_query("""SELECT 
+        c.customer_id,
+        c.first_name,
+        c.last_name,
+        COALESCE(SUM(p.amount), 0) AS total_paid,
+        
+        CASE 
+            WHEN COALESCE(SUM(p.amount), 0) >= 100 THEN 'High $$$'
+            WHEN COALESCE(SUM(p.amount), 0) BETWEEN 50 AND 99.99 THEN 'Medium $$'
+            WHEN COALESCE(SUM(p.amount), 0) BETWEEN 1 AND 49.99 THEN 'Low $'
+            ELSE 'No Payment'
+        END AS spending_tier
 
-# bhai kya tatti banaya hai maine
+    FROM customer c
+    LEFT JOIN payment p ON c.customer_id = p.customer_id
+    GROUP BY c.customer_id, c.first_name, c.last_name
+    ORDER BY total_paid DESC
+    LIMIT 5
+    """, cur, conn,"spending_tiers.csv")
+
+    cur.close()
+    conn.close()
+
+
+
+
+
+
+
+
+
+
+# hellll yeahh prod worthy babyyy
