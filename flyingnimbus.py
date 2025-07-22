284854d4090b8c22b64aaf8ad4f292366534b2b3
r-o-bo <ther0b0tombstone@gmail.com>
2025-07-23 01:12:03 +0530

    fix postgres connection

--- flyingnimbus.py
+++ flyingnimbus.py
@@ -8,7 +8,7 @@ load_dotenv()
 # connection with postgres
 def get_connection():
     conn = pg2.connect(
-        host=os.getenv('DB_HOST'),
+        host=os.getenv("DB_HOST", "localhost"),
         database=os.getenv('DB_NAME'),
         user=os.getenv('DB_USER'),
         password=os.getenv('DB_PASSWORD')
